db292e0dde3470dd38be17c4275f48a0186211d6
Tiago Jerônimo <tiago.batmansz@gmail.com>
2022-07-01 10:17:40 -0300

    [REQUISITO 6 - OUTRA TENTATIVA]

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,9 @@
-SELECT
+SELECT 
 	MIN(planos.valor) AS faturamento_minimo,
-	ROUND(MAX(planos.valor), 2) AS faturamento_maximo,
-    ROUND(AVG(planos.valor), 2) AS faturamento_medio,
-    ROUND(SUM(planos.valor),2 ) AS faturamento_total
+	MAX(planos.valor) AS faturamento_maximo,
+	ROUND(AVG(planos.valor), 2) AS faturamento_medio,
+	ROUND(SUM(planos.valor), 2) AS faturamento_total
 
-FROM SpotifyClone.`planos`
-	JOIN usuario ON planos.id_plano = usuario.id_plano;
+FROM SpotifyClone.planos AS planos
+INNER JOIN SpotifyClone.usuario AS usuario
+ON usuario.id_plano = planos.id_plano;
