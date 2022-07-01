087a86d32fcd2010d6b5c4aa77bbf996f22935b2
Tiago Jerônimo <tiago.batmansz@gmail.com>
2022-07-01 00:32:13 -0300

    fix [REQUISITO 5 - FEITO]

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+SELECT
+	musica.nome AS cancao,
+	COUNT(historico.id_musica) AS reproducoes
+    
+FROM SpotifyClone.musica
+	JOIN historico ON musica.id_musica = historico.id_musica
+    GROUP BY cancao
+    ORDER BY reproducoes DESC ,
+cancao LIMIT 2;
