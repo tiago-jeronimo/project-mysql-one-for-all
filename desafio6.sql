494e12e9e294fe4695563afab53aaca9de6dd1b3
Tiago Jerônimo <tiago.batmansz@gmail.com>
2022-07-01 00:54:10 -0300

    fix [REQUISITO 6 - CORREÇÃO]

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,6 @@
 SELECT
 	MIN(planos.valor) AS faturamento_minimo,
-	MAX(planos.valor) AS faturamento_maximo,
+	ROUND(MAX(planos.valor), 2) AS faturamento_maximo,
     ROUND(AVG(planos.valor), 2) AS faturamento_medio,
     ROUND(SUM(planos.valor),2 ) AS faturamento_total
     
